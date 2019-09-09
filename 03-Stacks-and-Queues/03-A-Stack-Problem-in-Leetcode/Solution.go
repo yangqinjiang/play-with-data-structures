a28266b1327533a2dffd6f2a1631fba52fa901f2
yangqinjiang-macbookpro <yangqinjiang@qq.com>
2019-09-09 11:51:13 +0800

    leetCode-20

--- 03-Stacks-and-Queues/03-A-Stack-Problem-in-Leetcode/Solution.go
+++ 03-Stacks-and-Queues/03-A-Stack-Problem-in-Leetcode/Solution.go
@@ -1,9 +1,39 @@
 package _3_A_Stack_Problem_in_Leetcode
-// TODO: leetCode的题目
+
+import (
+	"container/list"
+)
+
+// leetCode的题目 20
+//https://leetcode-cn.com/problems/valid-parentheses/submissions/
 func IsValid(s string) bool {
 	return isValid(s)
 }
 func isValid(s string) bool {
+	//使用list模拟Stack
+	stack := list.New()
+	for i:=0;i<len(s) ;i++  {
+		c := s[i]
 
-	return false
+		if c == '(' || c == '[' || c == '{'{
+			stack.PushBack(c)//压入
+		}else{
+			if 0 == stack.Len(){
+				return false
+			}
+			topChar := stack.Remove(stack.Back()).(uint8)//弹出
+			//topString := string(topChar)
+			//fmt.Println("pf - ",string(c),"-",topString)
+			if c == ')' && topChar != '('{
+				return false
+			}
+			if c == ']' && topChar != '['{
+				return false
+			}
+			if c == '}' && topChar != '{'{
+				return false
+			}
+		}
+	}
+	return stack.Len() == 0
 }
