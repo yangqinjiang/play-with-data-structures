5e41a2050b0cb5b4ac18ff0200f9711446e3dfda
yangqinjiang-macbookpro <yangqinjiang@qq.com>
2019-09-28 12:17:38 +0800

    查找后继节点,顶替 [删除的元素]

--- 06-Binary-Search-Tree/12-Remove-Elements-in-BST/main/main.go
+++ 06-Binary-Search-Tree/12-Remove-Elements-in-BST/main/main.go
@@ -3,22 +3,47 @@ package main
 import (
 	"fmt"
 	_12_Remove_Elements_in_BST "github.com/yangqinjiang/play-with-data-structures/06-Binary-Search-Tree/12-Remove-Elements-in-BST"
+	"math/rand"
+	"strconv"
+	"time"
 )
 
+func shuffle(arr []int)  {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i:= len(arr) -1;i>=0;i--{
+		pos := r.Int() %len(arr)
+		//fmt.Print(pos)
+		//fmt.Print(",")
+		//交换
+		arr[i],arr[pos] = arr[pos],arr[i]
+	}
+}
 func main() {
-	 bst := _12_Remove_Elements_in_BST.NewBST()
-	 nums := []int{5,3,6,8,4,2,1,10}
-	for _,num := range nums{
-		bst.Add(num)
+	bst := _12_Remove_Elements_in_BST.NewBST()
+
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	n := 10
+	order := make([]int,n)
+	for i:=0;i<n;i++{
+		// 注意, 由于随机生成的数据有重复, 所以bst中的数据数量大概率是小于n的
+		bst.Add(r.Intn(n))
+		// order数组中存放[0...n)的所有元素
+		order[i] = i
 	}
-	//fmt.Println(bst.ToString())
-	bst.Remove(3)
-	bst.Remove(6)
-	fmt.Println(bst.ToString())
-	if 1 != bst.Minimun(){
-		panic("minValue should be 1")
+	fmt.Println(order)
+	// 打乱order数组的顺序
+	shuffle(order)
+	fmt.Print("打乱order数组的顺序: ")
+	fmt.Println(order)
+	// 乱序删除[0...n)范围里的所有元素
+	for i:=0;i<n;i++{
+		if bst.Contains(order[i]){
+			bst.Remove(order[i])
+			fmt.Println("After remve "+ strconv.Itoa(order[i]) + " , size = " + strconv.Itoa(bst.Size()))
+		}
 	}
-	if 10 != bst.Maximun(){
-		panic("maxValue should be 10")
+	// 最终整个二分搜索树应该为空
+	if !bst.IsEmpty(){
+		panic("最终整个二分搜索树应该为空")
 	}
 }
