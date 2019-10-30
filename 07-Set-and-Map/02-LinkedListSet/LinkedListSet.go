fabbc59705b539e07f19a98c52572d9e92c5107e
yangqinjiang-macbookpro <yangqinjiang@qq.com>
2019-10-30 11:37:27 +0800

    基于linkedList的集合set的实现

---
+++ 07-Set-and-Map/02-LinkedListSet/LinkedListSet.go
@@ -0,0 +1,37 @@
+package _2_Linked_List_Set
+
+import Linked_List_Set "github.com/yangqinjiang/play-with-data-structures/07-Set-and-Map/02-LinkedListSet/LinkedListSet"
+
+/**
+集合类, 实现Set接口的LinkedList
+*/
+type LinkedListSet struct {
+	linkedList * Linked_List_Set.LinkedList //这个LinkedList的元素是string类型
+}
+
+func NewLinkedListSet() *LinkedListSet  {
+	return &LinkedListSet{linkedList: Linked_List_Set.NewLinkedList()}
+}
+//添加元素到集合内
+func (this *LinkedListSet)Add(e string)  {
+	if !this.linkedList.Contains(e){
+		this.linkedList.AddFirst(e) // 添加到链表头部
+	}
+
+}
+//查询集合内,是否存在元素e
+func (this *LinkedListSet)Contains(e string) bool{
+	return this.linkedList.Contains(e)
+}
+//从集合内,删除元素e
+func (this *LinkedListSet)Remove(e string){
+	this.linkedList.RemoveElement(e)
+}
+//返回集合的元素个数
+func (this *LinkedListSet)GetSize() int{
+	return this.linkedList.GetSize()
+}
+//判断集合是否为空
+func (this *LinkedListSet)IsEmpty() bool{
+	return this.linkedList.IsEmpty()
+}
---
+++ 07-Set-and-Map/02-LinkedListSet/main/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	_2_Linked_List_Set "github.com/yangqinjiang/play-with-data-structures/07-Set-and-Map/02-LinkedListSet"
+
+	"github.com/yangqinjiang/play-with-data-structures/Utils/FileOperation"
+	"path/filepath"
+)
+//读取文件的文本内容,添加到BST set集合内,并且统计不同的单词数量
+func main() {
+
+	//
+	fmt.Println("测试基于BST的集合,统计不同的单词数量")
+	readFileAndTotalDifferentWords("07-Set-and-Map/01-Set-Basics-and-BSTSet/pride-and-prejudice.txt",_2_Linked_List_Set.NewBSTSet())
+	fmt.Println("----------------------")
+	readFileAndTotalDifferentWords("07-Set-and-Map/01-Set-Basics-and-BSTSet/a-tale-of-two-cities.txt",_2_Linked_List_Set.NewBSTSet())
+	fmt.Println("----------------------")
+	fmt.Println("测试基于LinkedList的集合,统计不同的单词数量")
+	readFileAndTotalDifferentWords("07-Set-and-Map/01-Set-Basics-and-BSTSet/pride-and-prejudice.txt",_2_Linked_List_Set.NewLinkedListSet())
+	fmt.Println("----------------------")
+	readFileAndTotalDifferentWords("07-Set-and-Map/01-Set-Basics-and-BSTSet/a-tale-of-two-cities.txt",_2_Linked_List_Set.NewLinkedListSet())
+
+}
+
+func readFileAndTotalDifferentWords(path string,set _2_Linked_List_Set.Set)  {
+	if set == nil{
+		panic(" set can't be nil")
+	}
+	fmt.Printf("Read File: %s ....\n",path)
+
+	filename1, _ := filepath.Abs(path)
+	words1,err1 := FileOperation.ReadFile(filename1)
+	if err1 != nil{
+		return
+	}
+	fmt.Println("Total words: ",len(words1))
+	//set1 := _2_Linked_List_Set.NewBSTSet()
+	for _,word := range words1 {
+		set.Add(word)
+	}
+	fmt.Printf("Total different words: %d \n",set.GetSize())
+}
