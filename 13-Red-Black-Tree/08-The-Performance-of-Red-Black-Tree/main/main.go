976908ea441dbd490ead07af9424f4cf45f6a87c
yangqinjiang-macbookpro <yangqinjiang@qq.com>
2019-12-07 18:00:49 +0800

    红黑树的性能与AVLTree,BST的对比

--- 13-Red-Black-Tree/08-The-Performance-of-Red-Black-Tree/main/main.go
+++ 13-Red-Black-Tree/08-The-Performance-of-Red-Black-Tree/main/main.go
@@ -143,7 +143,7 @@ func main2()  {
 }
 
 //测试顺序数,在教程视频的耗时: RBTree < AVLTree,
-// TODO: 实际是 AVLTree < RBTree
+// TODO: BUG:?  实际是 AVLTree < RBTree
 func main3()  {
 	fmt.Println("main3")
 	n := 20000000
